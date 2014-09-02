6cdc302b27caff3fa3df13203cc4774d95d02c31
Alex Gill <alex.gill@penna.com>
2014-09-02 15:53:52 +0100

    Refactor (merged pairTwo function)

--- Countdown/Numbers.fs
+++ Countdown/Numbers.fs
@@ -25,23 +25,13 @@ let write n =
     write' n + " = " + (eval n).ToString()
 
 let generate exprs =
-    // a specialised choose2 function that returns the pair plus the remainders
-    // uses indices for comparison in case of duplicate values
-    let pairTwo exprs =
-        let indexed = List.mapi (fun i x -> i,x) exprs
-        [ for (i,x) in indexed do
-          for (j,y) in indexed do
-          if j > i then
-            yield (x,y), [ for (k,z) in indexed do if k <> i && k <> j then yield z ] ]
-
     // for two given expressions, get possible combinations
     let getExpressions x y =
         let isDivisible x y =
             let denom = eval y
             denom <> 0 && eval x % denom = 0
             
-        let a = max x y
-        let b = min x y
+        let a,b = if x > y then x,y else y,x
         seq {
             yield Multiply (x, y)
             yield Add (x, y)
@@ -52,10 +42,14 @@ let generate exprs =
             yield y // although expands the search tree
         }
     
+    let indexed = List.mapi (fun i x -> i,x) exprs
+
     seq {
-        for (x,y),zs in pairTwo exprs do
-        for expr in getExpressions x y do
-        yield expr :: zs }
+        for i,x in indexed do
+        for j,y in indexed do
+        if j > i then
+            for expr in getExpressions x y do
+            yield expr :: [ for k,z in indexed do if k <> i && k <> j then yield z ] }
 
 
 let compute numbers target =
@@ -69,8 +63,8 @@ let compute numbers target =
     let rec compute' candidates =
         let first = Seq.head candidates
         match first, fitness first with
-        | x::[], _ -> x // no more iterations, this is the best one
+        | x::[], _  -> x // no more iterations, this is the best one
         | exprs,  0 -> Seq.minBy distance exprs // woop, got it - now find which expression met the criteria
-        | _,     _ -> candidates |> iterate |> compute'
+        | _         -> candidates |> iterate |> compute'
     seq { yield numbers |> List.map Number }
     |> compute'
